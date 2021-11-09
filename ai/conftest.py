51b1a5b91930f4eb6e7fa5f637f67590a0a7479d
Julien <julien@MacBook-Pro-de-Julien.local>
2021-11-09 11:55:43 +0100

    initial commit

---
+++ ai/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ai.users.models import User
+from ai.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
