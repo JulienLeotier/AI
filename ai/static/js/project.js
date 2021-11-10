b04e6770d6571b0b15c4227eaf7e77840b357afb
JulienLeotier <julien.leotier@gmail.com>
2021-11-10 01:06:24 +0100

    ADD MENU

--- ai/static/js/project.js
+++ ai/static/js/project.js
@@ -1,1 +1,7 @@
 /* Project specific Javascript goes here. */
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
+
+console.log('hello')
